7f2e1b3f2b2a5ad5f3a3199e5d6988f41c717ea8
Indu Sahu <indusahu.npc@gmail.com>
2024-09-07 13:31:16 +0530

    file name are not correct

--- Array/Pascals_Triangle.cpp
+++ Array/Pascals_Triangle.cpp
@@ -1,4 +1,6 @@
 /*
+
+//Variation-1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -21,14 +23,17 @@ int pascalTriangle(int r, int c) {
 int main()
 {
     int r = 5;
-    int c = 3; 
+    int c = 3;
     int element = pascalTriangle(r, c);
     cout << "The element at position (r,c) is: "
             << element << "\n";
     return 0;
-}       
+}
 //Output: The element at position (r,c) is: 6
-*/
+
+
+//Variation-2
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -50,4 +55,53 @@ int main()
     int n = 5;
     pascalTriangle(n);
     return 0;
+}*/
+
+// Variation-3
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r)
+{
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++)
+    {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return (int)(res);
+}
+
+vector<vector<int>> pascalTriangle(int n)
+{
+    vector<vector<int>> ans;
+
+    // Store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++)
+    {
+        vector<int> tempLst; // temporary list
+        for (int col = 1; col <= row; col++)
+        {
+            tempLst.push_back(nCr(row - 1, col - 1));
+        }
+        ans.push_back(tempLst);
+    }
+    return ans;
+}
+
+int main()
+{
+    int n = 5;
+    vector<vector<int>> ans = pascalTriangle(n);
+    for (auto it : ans)
+    {
+        for (auto ele : it)
+        {
+            cout<< ele << "  ";
+        }
+        cout << "\n";
+    }
 }
