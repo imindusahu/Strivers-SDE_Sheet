2cc37925c39b88c7af14926c61b174c71ecd1026
Indu Sahu <indusahu.npc@gmail.com>
2024-09-07 12:57:44 +0530

    not run

---
+++ Array/Pascal's_Triangle.cpp
@@ -0,0 +1,53 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+
+int pascalTriangle(int r, int c) {
+    int element = nCr(r - 1, c - 1);
+    return element;
+}
+
+int main()
+{
+    int r = 5;
+    int c = 3; 
+    int element = pascalTriangle(r, c);
+    cout << "The element at position (r,c) is: "
+            << element << "\n";
+    return 0;
+}       
+//Output: The element at position (r,c) is: 6
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void pascalTriangle(int n) {
+    long long ans = 1;
+    cout << ans << " "; // printing 1st element
+
+    //Printing the rest of the part:
+    for (int i = 1; i < n; i++) {
+        ans = ans * (n - i);
+        ans = ans / i;
+        cout << ans << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n = 5;
+    pascalTriangle(n);
+    return 0;
+}
